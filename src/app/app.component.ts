2d1a07d224768a4bf2ce6c8a52decb4bd89fcf54
Montaniz Stills <57910061+montanizstills@users.noreply.github.com>
2020-06-07 16:21:47 -0400

    updated assets/; removedtemplate code; created linkservice to inject theming dependencies

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { LinkserviceService } from './linkservice.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  constructor(private linkservice: LinkserviceService) { }
+  ngOnInit() {
+    this.linkservice.addTag({ rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Poppins:200,300,400,600,700,800' });
+    this.linkservice.addTag({ rel: 'stylesheet', href: 'https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet' });
+  }
   title = 'angular-webapp';
 }
