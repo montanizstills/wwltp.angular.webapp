0a41bb500ea072f2f8f1f8ec08f93c2c220f17c3
Montaniz Stills <57910061+montanizstills@users.noreply.github.com>
2020-07-15 13:32:54 -0400

    un-nested hidden dropdown info

--- src/app/genres/genres.component.ts
+++ src/app/genres/genres.component.ts
@@ -11,12 +11,17 @@ import { AppState } from "./../app.state";
 })
 export class GenresComponent implements OnInit {
   genres: Observable<Genre>;
-  subgenres=[1,2,3];
-
+  subgenres = [1, 2, 3];
+  babygenre;
   constructor(private store: Store<AppState>) {
     //APICall, initialization
     this.genres = store.select("genre"); //bound to our reducer in app.module.ts 
-    console.log(this.genres)
+    // this.babygenre = this.genres.subscribe({
+    //   next(element){
+    //     console.log(element)
+    //   }
+    // });
+    
   }
 
   ngOnInit(): void { }
