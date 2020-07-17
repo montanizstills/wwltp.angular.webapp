d2bd035d0d4de1a6508e808ce0901ebbb327e0c3
Montaniz Stills <57910061+montanizstills@users.noreply.github.com>
2020-07-17 00:24:30 -0400

    updated navbar.actions to use createAction over class-based actions

--- src/app/actions/navbar.actions.ts
+++ src/app/actions/navbar.actions.ts
@@ -1,20 +1,4 @@
-import { Injectable } from "@angular/core";
-import { Action } from "@ngrx/store";
-import { Genre } from 'src/app/models/genre.model';
+import { Action, createAction, props } from "@ngrx/store";
 
-export const ADD_GENRE = '[Genre] Add';
-export const REMOVE_GENRE = "[Genre] Remove";
-
-
-export class AddGenre implements Action{
-    readonly type = ADD_GENRE;
-    constructor(public payload: Genre[]){}
-}
-
-export class RemoveGenre implements Action{
-    readonly type = REMOVE_GENRE;
-    constructor(public payload: Genre[]){}
-}
-
-//references actions, make a seperate file to reference all types of Actions.
-export type Actions = AddGenre | RemoveGenre
+export const ADD_GENRE = createAction("[Genre] Add",props<{key:string;value:string[]}>());
+export const REMOVE_GENRE = createAction("[Genre] Remove",props<{key:string}>());
