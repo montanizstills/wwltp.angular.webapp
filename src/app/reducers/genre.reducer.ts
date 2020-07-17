c7bb556e5f18471be49f861c966a1e1891ae5fd1
Montaniz Stills <57910061+montanizstills@users.noreply.github.com>
2020-07-17 11:11:02 -0400

    updated genre.reducer

--- src/app/reducers/genre.reducer.ts
+++ src/app/reducers/genre.reducer.ts
@@ -1,17 +1,21 @@
 import * as GenreActions from "../actions/navbar.actions";
 import { Action } from 'rxjs/internal/scheduler/Action';
+import { on, createReducer } from '@ngrx/store';
 
 //define default state.
-const defaultState:Map<String,String[]> = new Map();
+export const defaultState: Map<String, String[]> = new Map();
 defaultState.set("Puzzle", ["Soduku", "Rubix,", "Jenga"]);
 defaultState.set("Action", ["MarioWorld", "Enter the Matrix", "World of Warcraft"])
 
-export function GenreReducer(
-  state:Map<String,String[]> = defaultState,
-  action:Action) {
+const _genreReducerAddGenre = (k, v) => createReducer(defaultState, on(GenreActions.ADD_GENRE, defaultState => defaultState.set(k, v)));
+const _genreReducerRemoveGenre = k => createReducer(defaultState, on(GenreActions.REMOVE_GENRE, defaultState => {defaultState.delete(k); return defaultState}));
+
+export function GenreReducer(state: Map<String, String[]> = defaultState, action: Action.ADD_GENRE) {
   switch (action.type) {
     case GenreActions.ADD_GENRE:
-      return [...state, action.payload];
+      return _genreReducerAddGenre(state, action);
+    case GenreActions.REMOVE_GENRE:
+      return _genreReducerRemoveGenre(state, action)
     default:
       return state;
   }
