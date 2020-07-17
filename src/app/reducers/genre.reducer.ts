94ae7fccf2a7dbe9fcfb00f50d7102d3e1ffccca
Montaniz Stills <57910061+montanizstills@users.noreply.github.com>
2020-07-17 11:27:24 -0400

    updated genre.reducer

--- src/app/reducers/genre.reducer.ts
+++ src/app/reducers/genre.reducer.ts
@@ -7,16 +7,18 @@ export const defaultState: Map<String, String[]> = new Map();
 defaultState.set("Puzzle", ["Soduku", "Rubix,", "Jenga"]);
 defaultState.set("Action", ["MarioWorld", "Enter the Matrix", "World of Warcraft"])
 
-const _genreReducerAddGenre = (k, v) => createReducer(defaultState, on(GenreActions.ADD_GENRE, defaultState => defaultState.set(k, v)));
-const _genreReducerRemoveGenre = k => createReducer(defaultState, on(GenreActions.REMOVE_GENRE, defaultState => {defaultState.delete(k); return defaultState}));
+const _reducerAddGenre = (k, v) => createReducer(defaultState, on(GenreActions.ADD_GENRE, defaultState => defaultState.set(k, v)));
+const _reducerRemoveGenre = k => createReducer(defaultState, on(GenreActions.REMOVE_GENRE, defaultState => {
+  defaultState.delete(k); return defaultState
+}));
 
-export function GenreReducer(state: Map<String, String[]> = defaultState, action: Action.ADD_GENRE) {
+export function GenreReducer(state, action) {
   switch (action.type) {
     case GenreActions.ADD_GENRE:
-      return _genreReducerAddGenre(state, action);
+      return _reducerAddGenre(state, action);
     case GenreActions.REMOVE_GENRE:
-      return _genreReducerRemoveGenre(state, action)
+      return _reducerRemoveGenre(action)
     default:
-      return state;
+      return defaultState
   }
 }
