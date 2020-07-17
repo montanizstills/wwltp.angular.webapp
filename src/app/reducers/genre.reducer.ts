23d3fa1191b6ac85ee86f35c31c75bec334355fe
Montaniz Stills <57910061+montanizstills@users.noreply.github.com>
2020-07-17 15:02:14 -0400

    restructuring defaultState to be of type State<T>

--- src/app/reducers/genre.reducer.ts
+++ src/app/reducers/genre.reducer.ts
@@ -1,18 +1,35 @@
 import * as GenreActions from "../actions/navbar.actions";
-import { Action } from 'rxjs/internal/scheduler/Action';
-import { on, createReducer } from '@ngrx/store';
+import { on, createReducer, State, Action } from '@ngrx/store';
 
 //define default state.
-export const defaultState: Map<String, String[]> = new Map();
-defaultState.set("Puzzle", ["Soduku", "Rubix,", "Jenga"]);
-defaultState.set("Action", ["MarioWorld", "Enter the Matrix", "World of Warcraft"])
+export const defaultState: State<Map<String, String[]>> = 
+new State(GenreActions,GenreReducer,"",null)
 
-const _reducerAddGenre = (k:String, v:String[]) => createReducer(defaultState, on(GenreActions.ADD_GENRE, defaultState => defaultState.set(k, v)));
-const _reducerRemoveGenre = (k:String) => createReducer(defaultState, on(GenreActions.REMOVE_GENRE, defaultState => {
-  defaultState.delete(k); return defaultState
-}));
+defaultState.next({"":["",""]})
 
-export function GenreReducer(state, action) {
+// defaultState.set("Puzzle", ["Soduku", "Rubix,", "Jenga"]);
+// defaultState.set("Action", ["MarioWorld", "Enter the Matrix", "World of Warcraft"])
+
+const _reducerAddGenre = (k: String, v: String[]) =>
+  createReducer(
+    defaultState,
+    on(
+      GenreActions.ADD_GENRE,
+      defaultState => defaultState.set(k, v)
+    ));
+
+const _reducerRemoveGenre = (k: String) =>
+  createReducer(
+    defaultState,
+    on(
+      GenreActions.REMOVE_GENRE,
+      defaultState => {
+        defaultState.delete(k);
+        return defaultState
+      }
+    ));
+
+export function GenreReducer(state: State<Map<String, String[]>> = defaultState, action: Action) {
   switch (action.type) {
     case GenreActions.ADD_GENRE:
       return _reducerAddGenre(state, action);
