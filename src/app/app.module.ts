5058013f613ac7afad90f19a3af774b0f60d2e88
Montaniz Stills <57910061+montanizstills@users.noreply.github.com>
2020-07-20 13:08:42 -0400

    added @ngrx/router-store, @ngrx/devtools

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,13 +10,18 @@ import { GenreReducer } from "./reducers/genre.reducer";
 
 import { EntityDataModule } from '@ngrx/data';
 import { entityConfig } from './entity-metadata';
+
 import { EffectsModule } from '@ngrx/effects';
 import { AppEffects } from './app.effects';
+
+import { StoreRouterConnectingModule } from '@ngrx/router-store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent, GenresComponent],
   // forRoot reducers apply default/startup state
-  imports: [BrowserModule, AppRoutingModule, StoreModule.forRoot({genre:GenreReducer}), EntityDataModule.forRoot(entityConfig), EffectsModule.forRoot([AppEffects, AppEffects, ])],
+  imports: [BrowserModule, AppRoutingModule, StoreModule.forRoot({genre:GenreReducer}), EntityDataModule.forRoot(entityConfig), EffectsModule.forRoot([AppEffects, AppEffects, ]), StoreRouterConnectingModule.forRoot(), StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production })],
   providers: [],
   bootstrap: [AppComponent],
 })
