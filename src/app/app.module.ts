2099c13a5804a5e09a4e97ce004c407a5a71987f
Montaniz Stills <57910061+montanizstills@users.noreply.github.com>
2020-07-21 16:30:00 -0400

    updated app.module; created reducers in genres/index.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,11 @@ import { NgModule } from "@angular/core";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
-import {HttpClientModule} from "@angular/common/http"
+import { HttpClientModule } from "@angular/common/http"
 
 import { GenresComponent } from "./genres/genres.component";
-import * as fromGenre from './genres/genre.reducer';
 import * as fromGenreState from './genres';
+import { GenreEffects } from './genres/genre.effects';
 
 import { StoreModule } from "@ngrx/store";
 
@@ -22,28 +22,27 @@ import { StoreRouterConnectingModule } from '@ngrx/router-store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 import { environment } from '../environments/environment';
-import { GenreEffects } from './genres/genre.effects';
 
 @NgModule({
   declarations: [AppComponent, GenresComponent],
   // forRoot reducers apply default/startup state
-  imports: [BrowserModule, 
+  imports: [BrowserModule,
     AppRoutingModule,
-    HttpClientModule, 
-    // StoreModule.forRoot({}, {}),
+    HttpClientModule,
+
+    StoreModule.forRoot({}, {}),
     StoreModule.forFeature(fromGenreState.genreStateFeatureKey, fromGenreState.reducers, { metaReducers: fromGenreState.metaReducers }),
-    
-    EntityDataModule.forRoot(entityConfig), 
-    
-    EffectsModule.forRoot([AppEffects, AppEffects, ]),
+
+    EntityDataModule.forRoot(entityConfig),
+
+    EffectsModule.forRoot([AppEffects]),
     EffectsModule.forFeature([GenreEffects]),
-    
-    StoreRouterConnectingModule.forRoot(), 
-    
+
+    StoreRouterConnectingModule.forRoot(),
+
     StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }),
-    // StoreModule.forFeature(fromGenre.genresFeatureKey, fromGenre.reducer),
   ],
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
--- src/app/genres/index.ts
+++ src/app/genres/index.ts
@@ -9,6 +9,8 @@ import {
 } from '@ngrx/store';
 import { environment } from '../../environments/environment';
 import { Genre } from './genre.model';
+import { loadGenres } from './genre.actions';
+import { Action } from 'rxjs/internal/scheduler/Action';
 
 export const genreStateFeatureKey = 'genreState';
 
@@ -17,9 +19,16 @@ export interface GenreState {
   error: any
 }
 
-export const defaultState: GenreState = {
+export const initialState: GenreState = {
   genres: undefined,
   error: undefined
 }
+
+export const reducers =createReducer(
+  initialState,
+  on(loadGenres,(state,action)=>{
+    return {genres:action.genres}
+  })
+)
 
 export const metaReducers: MetaReducer<GenreState>[] = !environment.production ? [] : [];
