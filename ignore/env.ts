6a6dde02942ec9b700c3dd4b0020cbb609e841bd
Montaniz Stills <57910061+montanizstills@users.noreply.github.com>
2020-09-21 14:58:37 -0400

    npm install --save-dev jest

---
+++ ignore/env.ts
@@ -0,0 +1,9 @@
+export const TWITCH_CLIENT_ID = "84lelqn0a9uxd5dqi7o1lje8gvmi8n";
+export const TWITCH_CLIENT_SECRET = "7rlypspysgw8shrhzqnsv17rk0eotk"
+export const FACEBOOK_APP_ID = "223745635693472"
+export const FACEBOOK_APP_SECRET = "528e3d315db3fbb0c0441766e61d8ed6"
+export const AUTH0_CLIENT_ID = "GKSr9tqNB3jZ7lTKcPRyV8ktTXmjVIFD"
+export const AUTH0_CLIENT_SECRET = "YWgfn77fOBf26Z-XdDLVizLk-WDV9LUNfXP3_89HBW2o6tLoyIVpjxjxjTnDKZh8"
+export const YOUTUBE_API_KEY = "AIzaSyDjGj9eVcS5oISY7tjFaG0CEXwvc4JRGX8"
+export const GOOGLE_CLIENT_ID="775447916815-tu7k5a83m6hu7cffp2ppi09ce5evcubh.apps.googleusercontent.com"
+export const GOOGLE_CLIENT_SECRET="zPMKrANe700SBImc2MSGX1hR"
