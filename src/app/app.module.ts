6065f3a34f4d10370d0633ea48edbfb0348e6c71
Montaniz Stills <57910061+montanizstills@users.noreply.github.com>
2020-10-05 16:24:24 -0400

    created navbar, footer, and mainpage components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MainpageComponent } from './components/mainpage/mainpage/mainpage.component';
-import { NavbarComponent } from './components/navbar/navbar/navbar.component';
+import { MainpageComponent } from './components/mainpage/mainpage.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainpageComponent,
-    NavbarComponent
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
